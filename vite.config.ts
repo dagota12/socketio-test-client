c9bc123d1ff41889d2af7019df22c02ded0fff6f
dagota12 <chernetdagim@gmail.com>
2025-09-22 22:54:07 +0300

    Add preview configuration with allowed hosts for socketio-test-client

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      preview: {
+        allowedHosts: [
+          'socketio-test-client-eze4ehcjhngeeec0.italynorth-01.azurewebsites.net'
+        ]
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
